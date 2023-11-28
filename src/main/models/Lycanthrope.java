38129f06867e3ad813e0879f4ff9cef823c4ccdc
krokmou <pierrick.caron@etu.univ-amu.fr>
2023-11-29 00:47:12 +0100

    Ajout du constructeur

--- src/main/models/Lycanthrope.java
+++ src/main/models/Lycanthrope.java
@@ -1,6 +1,7 @@
 package main.models;
 
 public class Lycanthrope {
+    private String nom;
     private String sexe;
     private String categorieAge;
     private int force;
@@ -9,4 +10,17 @@ public class Lycanthrope {
     private int niveau;
     private int facteurImpetuosite;
     private String meute;
+
+    // Constructeur
+    public Lycanthrope(String nom, String sexe, String categorieAge, int force, int facteurDomination, int rangDomination, int facteurImpetuosite, String meute) {
+        this.nom = nom;
+        this.sexe = sexe;
+        this.categorieAge = categorieAge;
+        this.force = force;
+        this.facteurDomination = facteurDomination;
+        this.rangDomination = rangDomination;
+        this.niveau = calculerNiveau(); // Calcul du niveau en fonction des caractéristiques
+        this.facteurImpetuosite = facteurImpetuosite;
+        this.meute = meute;
+    }
 }
