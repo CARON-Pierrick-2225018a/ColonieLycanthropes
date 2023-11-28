cd4467162291e4036183b7fda98ca719ebb16a6a
krokmou <pierrick.caron@etu.univ-amu.fr>
2023-11-29 00:43:45 +0100

    Ajout des attributs

---
+++ src/main/models/Lycanthrope.java
@@ -0,0 +1,12 @@
+package main.models;
+
+public class Lycanthrope {
+    private String sexe;
+    private String categorieAge;
+    private int force;
+    private int facteurDomination;
+    private int rangDomination;
+    private int niveau;
+    private int facteurImpetuosite;
+    private String meute;
+}
