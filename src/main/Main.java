cc3ace8da62da1e3657fa7856df912b8fac55e8a
krokmou <pierrick.caron@etu.univ-amu.fr>
2023-11-29 00:59:09 +0100

    Retest tout fonctionne

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,19 +1,13 @@
 package main;
-import main.models.Lycanthrope;
+
+import main.controllers.LycanthropeController;
 
 public class Main {
-        public static void main(String[] args) {
-            // Création d'un lycanthrope
-            Lycanthrope lycanthrope1 = new Lycanthrope("GardeNuit", "Mâle", "Adulte", 80, 5, 2, 7, "MeuteAlpha");
+    public static void main(String[] args) {
+        // Création d'une instance du contrôleur
+        LycanthropeController lycanthropeController = new LycanthropeController();
 
-            // Affichage des informations du lycanthrope
-            System.out.println(lycanthrope1.toString());
-
-            // Modification de quelques caractéristiques du lycanthrope
-            lycanthrope1.setForce(90);
-            lycanthrope1.setRangDomination(3);
-
-            // Affichage des nouvelles informations du lycanthrope
-            System.out.println("\nAprès modification :\n" + lycanthrope1.toString());
-        }
+        // Appel de la méthode principale du contrôleur
+        lycanthropeController.main(args);
     }
+}
