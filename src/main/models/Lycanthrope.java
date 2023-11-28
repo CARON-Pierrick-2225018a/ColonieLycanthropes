74b0c3d0fe8ea9f7a0fbfb0e45d59bc7ababe441
krokmou <pierrick.caron@etu.univ-amu.fr>
2023-11-29 00:48:06 +0100

    Ajout des getters/setters

--- src/main/models/Lycanthrope.java
+++ src/main/models/Lycanthrope.java
@@ -23,4 +23,76 @@ public class Lycanthrope {
         this.facteurImpetuosite = facteurImpetuosite;
         this.meute = meute;
     }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getSexe() {
+        return sexe;
+    }
+
+    public void setSexe(String sexe) {
+        this.sexe = sexe;
+    }
+
+    public String getCategorieAge() {
+        return categorieAge;
+    }
+
+    public void setCategorieAge(String categorieAge) {
+        this.categorieAge = categorieAge;
+    }
+
+    public int getForce() {
+        return force;
+    }
+
+    public void setForce(int force) {
+        this.force = force;
+    }
+
+    public int getFacteurDomination() {
+        return facteurDomination;
+    }
+
+    public void setFacteurDomination(int facteurDomination) {
+        this.facteurDomination = facteurDomination;
+    }
+
+    public int getRangDomination() {
+        return rangDomination;
+    }
+
+    public void setRangDomination(int rangDomination) {
+        this.rangDomination = rangDomination;
+    }
+
+    public int getNiveau() {
+        return niveau;
+    }
+
+    public void setNiveau(int niveau) {
+        this.niveau = niveau;
+    }
+
+    public int getFacteurImpetuosite() {
+        return facteurImpetuosite;
+    }
+
+    public void setFacteurImpetuosite(int facteurImpetuosite) {
+        this.facteurImpetuosite = facteurImpetuosite;
+    }
+
+    public String getMeute() {
+        return meute;
+    }
+
+    public void setMeute(String meute) {
+        this.meute = meute;
+    }
 }
