3df43df86f9d5c80617b741945ff0532fef2751d
krokmou <pierrick.caron@etu.univ-amu.fr>
2023-11-29 09:58:10 +0100

    Préparer le menu pour qu'on puisse avoir accès au menu d'action

--- src/main/controllers/LycanthropeController.java
+++ src/main/controllers/LycanthropeController.java
@@ -18,7 +18,8 @@ public class LycanthropeController {
             System.out.println("1. Afficher les informations du lycanthrope");
             System.out.println("2. Ajouter un nouveau loup-garou");
             System.out.println("3. Choisir un loup-garou");
-            System.out.println("4. Quitter");
+            System.out.println("4. Afficher le menu d'action");
+            System.out.println("5. Quitter");
 
             System.out.print("Faites votre choix : ");
             choix = scanner.nextInt();
@@ -34,6 +35,9 @@ public class LycanthropeController {
                     Lycanthrope.choisirLoupGarou();
                     break;
                 case 4:
+                    //Lycanthrope.afficherMenuActions(lycanthrope);
+                    break;
+                case 5:
                     System.out.println("Au revoir !");
                     break;
                 default:
