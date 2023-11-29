b81060fda67558a3c7904126d8bdaf529f869e7b
krokmou <pierrick.caron@etu.univ-amu.fr>
2023-11-29 01:06:06 +0100

    Modification pour que toute les méthodes soit dans le models

--- src/main/controllers/LycanthropeController.java
+++ src/main/controllers/LycanthropeController.java
@@ -5,14 +5,9 @@ import main.models.Lycanthrope;
 import java.util.Scanner;
 
 public class LycanthropeController {
-    private static Lycanthrope lycanthrope; // L'instance de la classe Lycanthrope
     private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        // Initialisation d'un lycanthrope (vous pouvez ajouter votre propre logique ici)
-        lycanthrope = new Lycanthrope("GardeNuit", "Mâle", "Adulte", 80, 5, 2, 7, "MeuteAlpha");
-
-        // Affichage du menu
         afficherMenu();
     }
 
@@ -21,25 +16,25 @@ public class LycanthropeController {
         do {
             System.out.println("\nMenu Lycanthrope");
             System.out.println("1. Afficher les informations du lycanthrope");
-            System.out.println("2. Quitter");
+            System.out.println("2. Ajouter un nouveau loup-garou");
+            System.out.println("3. Quitter");
 
             System.out.print("Faites votre choix : ");
             choix = scanner.nextInt();
 
             switch (choix) {
                 case 1:
-                    afficherInformationsLycanthrope();
+                    Lycanthrope.afficherInformationsLycanthropes();
                     break;
                 case 2:
+                    Lycanthrope.ajouterLoupGarou();
+                    break;
+                case 3:
                     System.out.println("Au revoir !");
                     break;
                 default:
                     System.out.println("Choix invalide. Veuillez réessayer.");
             }
-        } while (choix != 2);
-    }
-
-    private static void afficherInformationsLycanthrope() {
-        System.out.println(lycanthrope.toString());
+        } while (choix != 3);
     }
 }
