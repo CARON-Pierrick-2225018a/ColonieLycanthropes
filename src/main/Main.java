3691a182c73e33fa401b6257bd39d4a88b5c87ea
Killian Ferrier <killian.ferrier@etu.univ-amu.fr>
2023-12-04 09:23:24 +0100

    Factorisation de code

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,10 +4,7 @@ import main.controllers.LycanthropeController;
 
 public class Main {
     public static void main(String[] args) {
-        // Création d'une instance du contrôleur
-        LycanthropeController lycanthropeController = new LycanthropeController();
-
         // Appel de la méthode principale du contrôleur
-        lycanthropeController.main(args);
+        LycanthropeController.afficherMenu();
     }
 }
