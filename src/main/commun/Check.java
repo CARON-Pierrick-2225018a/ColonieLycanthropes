15b4b6db796e3c351fa49b3148f8e6fd45ea563a
Killian Ferrier <killian.ferrier@etu.univ-amu.fr>
2023-12-04 09:22:41 +0100

    Ajout d'une view (MVC) + Ajout d'une classe de vérification

---
+++ src/main/commun/Check.java
@@ -0,0 +1,29 @@
+package main.commun;
+
+import main.views.MenuView;
+
+import java.util.Scanner;
+
+public class Check {
+    static Scanner sc = new Scanner(System.in);
+    /**
+     * Vérifie si l'entrée utilisateur est un entier.
+     *
+     * @return L'entier saisi par l'utilisateur.
+     */
+    public static int checkIfEntreeIsInt() {
+        int entree;
+        while (true) {
+            try {
+                String input = sc.nextLine();
+                entree = Integer.parseInt(input);
+                break;
+            } catch (NumberFormatException e) {
+                // Si la conversion échoue, afficher un message d'erreur et afficher à nouveau le menu en boucle
+                System.out.println("Numéro entré invalide");
+                MenuView.menuView();
+            }
+        }
+        return entree;
+    }
+}
---
+++ src/main/views/MenuView.java
@@ -0,0 +1,14 @@
+package main.views;
+
+public class MenuView {
+    public static void menuView() {
+        System.out.println("\nMenu Lycanthrope");
+        System.out.println("1. Afficher les informations du lycanthrope");
+        System.out.println("2. Ajouter un nouveau loup-garou");
+        System.out.println("3. Choisir un loup-garou");
+        System.out.println("4. Afficher le menu d'action");
+        System.out.println("0. Quitter");
+
+        System.out.print("Faites votre choix : ");
+    }
+}
