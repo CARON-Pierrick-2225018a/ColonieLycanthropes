800e37ca26e6dbd09f180ea7373eccd0cfcf3e82
Killian Ferrier <killian.ferrier@etu.univ-amu.fr>
2023-12-05 19:13:53 +0100

    Ajout d'autres lycanthropes pour tests

--- src/main/initialize/InitializeData.java
+++ src/main/initialize/InitializeData.java
@@ -11,10 +11,13 @@ public class InitializeData {
         new Lycanthrope("Pierrick", "M", "Adulte", 0, 0, 0, 0);
         new Lycanthrope("Mona", "F", "Adulte", 0, 0, 0, 0);
         new Lycanthrope("Killian", "M", "Adulte", 0, 0, 1, 0);
+        new Lycanthrope("Vanessa", "F", "Adulte", 0, 0, 0, 0);
+        new Lycanthrope("Tom", "M", "Adulte", 0, 0, 0, 0);
+        new Lycanthrope("Fabio", "M", "Adulte", 0, 0, 0, 0);
 
         List<Lycanthrope> coupleAlpha = new ArrayList<>(List.of(Lycanthrope.getInstancesLycanthropes().get(0),Lycanthrope.getInstancesLycanthropes().get(1)));
 
-        new Meute("Première Meute", coupleAlpha, null);
+        new Meute("Meute des devs", coupleAlpha, null);
 
         Meute.getInstancesMeutes().get(0).ajouterUnLycanthropeALaMeute(Lycanthrope.getInstancesLycanthropes().get(2));
 
