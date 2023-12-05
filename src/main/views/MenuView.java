25a2efa7f0705a058090711fc69f6eba6d8a44de
Killian Ferrier <killian.ferrier@etu.univ-amu.fr>
2023-12-05 19:15:01 +0100

    Ajout du menu d'action pour les meutes

--- src/main/views/MenuView.java
+++ src/main/views/MenuView.java
@@ -2,6 +2,7 @@ package main.views;
 
 import main.initialize.InitializeData;
 import main.models.Lycanthrope;
+import main.models.Meute;
 
 import java.util.Objects;
 import java.util.Scanner;
@@ -11,24 +12,38 @@ public class MenuView {
         System.out.println("\nMenu Lycanthrope");
         System.out.println("1. Afficher la liste des lycanthropes");
         System.out.println("2. Afficher les informations d'un lycanthrope");
-        System.out.println("3. Ajouter un nouveau loup-garou");
-        System.out.println("4. Choisir un loup-garou");
-        System.out.println("5. Afficher le menu d'action");
-        System.out.println("6. Afficher la liste des meutes");
+        System.out.println("3. Ajouter un nouveau lycanthrope");
+        System.out.println("4. Choisir un lycanthrope");
+        System.out.println("5. Afficher la liste des meutes");
+        System.out.println("6. Afficher les informations d'une meute");
+        System.out.println("7. Ajouter une nouvelle meute");
+        System.out.println("8. Choisir une meute");
         System.out.println("0. Quitter");
 
         System.out.print("Faites votre choix : ");
     }
 
-    public static void afficherMenuAction(Lycanthrope lycanthrope) {
+    public static void afficherMenuActionPourLycanthropes(Lycanthrope lycanthrope) {
         System.out.println("\nMenu d'actions pour " + lycanthrope.getNom());
         System.out.println("1. Hurler pour communiquer ");
         System.out.println("2. Entendre un hurlement");
-        System.out.println("3. Afficher les données du loup ");
+        System.out.println("3. Afficher les informations du lycanthrope ");
         System.out.println("4. Séparer de la meute ");
         System.out.println("5. Se transformer en humain ");
-        System.out.println("6. Se transformer en loup  ");
-        System.out.println("7. Choisir un autre Loup ");
+        System.out.println("6. Se transformer en lycanthrope  ");
+        System.out.println("7. Choisir un autre lycanthrope ");
+        System.out.println("0. Retour au menu principal");
+
+        System.out.print("Faites votre choix : ");
+    }
+
+    public static void afficherMenuActionPourMeutes(Meute meute) {
+        System.out.println("\nMenu d'actions pour meute " + meute.getNom());
+        System.out.println("1. Afficher les informations de la meute ");
+        System.out.println("2. Ajouter un lycanthrope ");
+        System.out.println("3. Enlever un lycanthrope ");
+        System.out.println("4. Mettre un lycanthrope au rang ω ");
+        System.out.println("5. Choisir une autre meute ");
         System.out.println("0. Retour au menu principal");
 
         System.out.print("Faites votre choix : ");
