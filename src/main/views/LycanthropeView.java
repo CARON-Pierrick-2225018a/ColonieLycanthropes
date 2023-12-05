ad062b68ed69d8531ef728a839fefac10af2234e
Killian Ferrier <killian.ferrier@etu.univ-amu.fr>
2023-12-05 19:14:33 +0100

    Ajout de plusieurs méthodes d'affichage des lycanthropes

--- src/main/views/LycanthropeView.java
+++ src/main/views/LycanthropeView.java
@@ -3,39 +3,55 @@ package main.views;
 import main.common.Check;
 import main.models.Lycanthrope;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class LycanthropeView {
-    // Méthode d'action spécifique du lycanthrope (exemple)
-    public static void afficherDonner(Lycanthrope lycanthropeChoisi) {
-        if (lycanthropeChoisi != null) {
-            System.out.println(lycanthropeChoisi);
-        } else {
-            System.out.println("Aucun lycanthrope choisi.");
-        }
-    }
-
-    public static void afficherListeLycanthropes() {
-        if (Lycanthrope.getInstancesLycanthropes().isEmpty()) {
+    public static void afficherListeLycanthropes(List<Lycanthrope> listLycanthrope) {
+        if (listLycanthrope.isEmpty()) {
             System.out.println("Aucun lycanthrope à afficher.");
         } else {
             System.out.println("Liste des lycanthropes :");
-            Lycanthrope lycanthrope;
-            for (int i = 0; i < Lycanthrope.getInstancesLycanthropes().size(); i++) {
-                lycanthrope = Lycanthrope.getInstancesLycanthropes().get(i);
-                System.out.println(i + 1 + ". " + lycanthrope.getNom() + " (" + lycanthrope.getSexe() + ")");
+            for (int i = 0; i < listLycanthrope.size(); i++) {
+                System.out.println("\t"+ (i + 1) + ". " + listLycanthrope.get(i).getNom() + " (" + listLycanthrope.get(i).getSexe() + ")" +
+                        "(" + listLycanthrope.get(i).getRangDomination() + ")");
             }
         }
     }
 
     public static Lycanthrope choisirLoupGarou() {
-        List<Lycanthrope> instancesLycanthropes = Lycanthrope.getInstancesLycanthropes();
-        if (!instancesLycanthropes.isEmpty()) {
-            LycanthropeView.afficherListeLycanthropes();
+        List<Lycanthrope> listLycanthropes = Lycanthrope.getInstancesLycanthropes();
+        return choisirLoupGarouDeLaListe(listLycanthropes);
+    }
+
+    public static Lycanthrope choisirLoupGarouSansMeute() {
+        List<Lycanthrope> listLycanthropes = new ArrayList<>();
+        for (Lycanthrope lycanthrope : Lycanthrope.getInstancesLycanthropes()) {
+            if (Objects.equals(lycanthrope.getMeute(), null)){
+                listLycanthropes.add(lycanthrope);
+            }
+        }
+        return choisirLoupGarouDeLaListe(listLycanthropes);
+    }
+
+    public static Lycanthrope choisirLoupGarouSelonSonSex(String sexe) {
+        List<Lycanthrope> listLycanthropes = new ArrayList<>();
+        for (Lycanthrope lycanthrope : Lycanthrope.getInstancesLycanthropes()) {
+            if (Objects.equals(lycanthrope.getMeute(), null) && Objects.equals(lycanthrope.getSexe(), sexe)){
+                listLycanthropes.add(lycanthrope);
+            }
+        }
+        return choisirLoupGarouDeLaListe(listLycanthropes);
+    }
+
+    public static Lycanthrope choisirLoupGarouDeLaListe(List<Lycanthrope> listLycanthropes) {
+        if (!listLycanthropes.isEmpty()) {
+            LycanthropeView.afficherListeLycanthropes(listLycanthropes);
 
             int choixLoupGarou;
 
-            if (instancesLycanthropes.size() == 1) {
+            if (listLycanthropes.size() == 1) {
                 // S'il n'y a qu'un lycanthrope, le choisissez automatiquement
                 choixLoupGarou = 1;
                 System.out.println("Il n'y a qu'un lycanthrope disponible. Choix automatique.");
@@ -44,9 +60,9 @@ public class LycanthropeView {
                 choixLoupGarou = Check.checkIfEntreeIsInt();
             }
 
-            if (choixLoupGarou >= 1 && choixLoupGarou <= instancesLycanthropes.size()) {
+            if (choixLoupGarou >= 1 && choixLoupGarou <= listLycanthropes.size()) {
                 // L'utilisateur a choisi un lycanthrope valide
-                Lycanthrope lycanthropeChoisi = instancesLycanthropes.get(choixLoupGarou - 1);
+                Lycanthrope lycanthropeChoisi = listLycanthropes.get(choixLoupGarou - 1);
                 // Ajoutez ici le code pour effectuer d'autres actions avec le lycanthrope choisi
                 System.out.println("Le lycanthrope choisit est : " + lycanthropeChoisi.getNom());
                 return lycanthropeChoisi;
